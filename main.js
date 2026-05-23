abff0951b03262d57d744001f9756c437d678863
819605834 <124411012+819605834@users.noreply.github.com>
2026-05-23 05:31:21 +0000

    Created calculate bill function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const TAX_RATE = 0.095;
+const TIP_RATE = 0.2;
+function calculateBillTotal(bill){
+    return (bill + bill * TAX_RATE + bill * TIP_RATE).toFixed(2);
+}
 
+var total = calculateBillTotal(20);
+console.log(total);
